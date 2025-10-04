c6d7d751e459e6edd319eb3a3476b2f3208e93f6
ParkSumin <fhsjdvs@gmail.com>
2025-10-04 21:21:40 +0900

    fix:Main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import App from "./pages/App";
 
-createRoot(document.getElementById("root")).render(
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
